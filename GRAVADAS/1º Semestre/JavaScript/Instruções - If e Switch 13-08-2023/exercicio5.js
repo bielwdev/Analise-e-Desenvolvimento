f987b686b411cf0894a99f836890628407174253
Gabriel Victor <141093547+bielwdev@users.noreply.github.com>
2023-08-13 10:47:06 -0300

    Add files via upload

---
+++ GRAVADAS/1º Semestre/JavaScript/Instruções - If e Switch 13-08-2023/exercicio5.js
@@ -0,0 +1,15 @@
+var diaSem = 4;
+
+switch(diaSem){
+    case 1:
+        console.log("Hoje é Domingo");
+        break;
+    case 2:
+        console.log("Hoje é segunda-feira");
+        break;
+    case 3:
+        console.log("Hoje é terça-feira");
+        break;
+    default:
+        console.log("Esse dia da semana não existe")
+}
