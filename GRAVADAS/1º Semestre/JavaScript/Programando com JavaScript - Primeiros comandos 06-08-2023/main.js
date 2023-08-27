117b9dd561d17046891266866b315c6d3f547eac
bielwdev <bielwdev@gmail.com>
2023-08-27 00:19:41 -0300

    old archives

---
+++ GRAVADAS/1º Semestre/JavaScript/Programando com JavaScript - Primeiros comandos 06-08-2023/main.js
@@ -0,0 +1,9 @@
+var nome = "Gabriel";
+console.log(typeof(nome))
+
+if(typeof(nome) == "string"){
+    console.log(nome.toLocaleUpperCase())
+}
+else{
+    console.log("Você precisar usar uma string")
+}
