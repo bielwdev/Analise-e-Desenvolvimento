090bc2ef96b87ad211c3db03421faa44a5b48d29
bielwdev <bielwdev@gmail.com>
2023-09-03 13:26:49 -0300

    Strings e seus métodos

---
+++ 1º Semestre/JavaScript/Strings 03-09-2023/strv1.js
@@ -0,0 +1,23 @@
+var nome = "Gabriel Victor";
+var x = nome.length;
+console.log (x);
+
+resultado = x > 13 ? "Obrigado" : "Por favor preencha seu nome completo";
+
+console.log(resultado);
+
+var res = nome.toLocaleUpperCase();
+console.log(res);
+
+var res2 = nome.toLocaleLowerCase();
+console.log(res2)
+
+var jogo = "Barcelona vs Real melhor time";
+var posicao = jogo.indexOf("Real");
+console.log(posicao);
+
+var cortado = jogo.slice(0,17)
+console.log(cortado)
+
+var cortado2 = jogo.slice(13)
+console.log(cortado2)
