b37c593afdb219de727cde14d27d015687bab964
bielwdev <bielwdev@gmail.com>
2023-09-07 20:43:35 -0300

    aula Como utilizar?

---
+++ 1º Semestre/JavaScript/Promise 07-09-2023/ex2.js
@@ -0,0 +1,20 @@
+function somarArray(array){
+    return new Promise((resolve,reject) =>{
+        if(!Array.isArray(array)){
+            reject("O argumento passado não é um array")
+        }
+
+        const soma = array.reduce((total,num) => total + num,0);
+        resolve(soma)
+    })
+}
+
+const myArray = [1,2,3,4,5,6,7,8,9,10];
+
+somarArray(myArray)
+.then((resultado)=>{
+    console.log("A soma dos elementos é:", resultado)
+})
+.catch((erro) =>{
+    console.log("Ocorreu um erro:", erro)
+})
