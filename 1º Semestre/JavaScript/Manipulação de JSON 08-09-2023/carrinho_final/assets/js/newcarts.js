e12dc64f079469bc82068665c316b409e9781023
bielwdev <bielwdev@gmail.com>
2023-09-08 23:17:57 -0300

    aula Avançando com o uso JSON

--- 1º Semestre/JavaScript/Manipulação de JSON 08-09-2023/carrinho_final/assets/js/newcarts.js
+++ 1º Semestre/JavaScript/Manipulação de JSON 08-09-2023/carrinho_final/assets/js/newcarts.js
@@ -37,30 +37,53 @@ function adicionarAoCarrinho(nome, imgLink, descricao, preco) {
 
 function atualizarCarrinho() {
     let carrinhoHTML = document.getElementById("carrinho");
-    
+
     carrinho.forEach((produto,index) =>{
-        carrinhoHTML.innerHTML += `
-            <div id="produto${index}" class="d-flex flex-row justify-content-between align-items-center pt-lg-12 pt-md-12 pt-2 pb-3 border-bottom mobile">
-                <div class='d-flex flex-row align-items-center'>
-                    <div>
-                        <img src='${produto.imgLink}' width='150' height='150' id='image'/>
-                    </div>
-                    <div class='d-flex flex-column pl-md-4 pl-1'>
-                        <div><h6>${produto.nome}</h6></div>
-                    </div>
+        carrinhoHTML.innerHTML +=
+        `<div
+        id="produto${index}"
+        class="d-flex flex-row justify-content-between align-items-center pt-lg-12 pt-md-12 pt-2 pb-3 border-botton mobile">
+        <div class='d-flex flex-row align-items-center'>
+            <div>
+                <img
+                    src='${produto.imgLink}'
+                        width='150'
+                        height='150'
+                        id='image' 
+                    />
                 </div>
-                <div class='pl-md-2 pl-1'><b> R$ ${produto.preco}</b></div>
-                <div class='pl-md-3 pl-2'>
-                    <span class='fa fa-minus-square text-secondary' onclick='removeItem(${index})'></span>
-                    <span class='px-md-3 px-1' id='quantidade${index}'>${produto.quantidade}</span>
-                    <span class='fa fa-plus-square text-secondary' onclick='adicionarItem(${index})'></span>
+                <div class='d-flex flex-column pl-md-4 pl-1'>
+                    <div><h6>${produto.nome}</h6></div>
                 </div>
-                <div class='pl-md-0 pl-1'>
-                    <b>R$ <span id='total${index}'>${produto.total.toFixed(2)}</span></b>
-                </div>
-                <div class='close' onclick='removeProduto(${index})'></div>
-            </div>`;
-    });
+            </div>
+            <div class='pl-md-2 pl-1'><b> R$ ${produto.preco}</b></div>
+            <div class='pl-md-3 pl-2'>
+                <span
+                    class='fa fa-minus-square text-secondary'
+                    onclick='removeItem(${index})'
+                    >
+                </span>
+                    <span class='px-md-3 px-1' id='quantidade${index}'>
+                        ${produto.quantidade}
+                    </span>
+                <span
+                    class='fa fa-plus-square text-secondary'
+                    onclick='adicionarItem(${index})'
+                    >
+                </span>
+            </div>
+            <div class='pl-md-0 pl-1'>
+                <b>R$ <span id='total${index}'>0</span></b>
+            </div>
+
+            <div class='close' onclick='removeProduto(${index})'></div>
+        </div>`
+
+
+
+
+    })   
+
 }
 
 function adicionarItem(item){
@@ -115,5 +138,3 @@ function removeProduto(item){
         carrinhoHTML.removeChild(produto)
     }
 }
-
-
