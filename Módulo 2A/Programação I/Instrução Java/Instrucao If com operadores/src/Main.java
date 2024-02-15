3e16fce75a178587413c4027f65f52c0668b5571
Gabriel <bielwdev@gmail.com>
2024-02-15 18:55:20 -0300

    Instrução If com operadores

---
+++ Módulo 2A/Programação I/Instrução Java/Instrucao If com operadores/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int valor = 7;
+
+        if(valor == 8)
+            System.out.println("Valor = 8");
+        else
+            System.out.println("Valor != 8");
+
+        int valor2 = 5;
+
+        if(valor2 == 5)
+            System.out.println("Valor = 5");
+        else
+            System.out.println("Valor != 5");
+
+    }
+}
---
+++ Módulo 2A/Programação I/Instrução Java/Instrucao If com operadores/src/Main2.java
@@ -0,0 +1,14 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        int valor = 9;
+
+        if (valor == 8) {
+            System.out.println("Valor = 8");
+        } else if (valor > 8) {
+            System.out.println("Valor > 8");
+        } else {
+            System.out.println("Valor < 8");
+        }
+    }
+}
