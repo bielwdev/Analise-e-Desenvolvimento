b299caf3ccec31d8581888919b7e72f4e80df1de
Gabriel <bielwdev@gmail.com>
2024-02-15 18:23:18 -0300

    Operadores relacionais

---
+++ Módulo 2A/Programação I/Instrução Java/Operadores relacionais/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int valor1, valor2;
+
+        valor1 = 10;
+        valor2 = 5;
+
+        System.out.println(valor1 == valor2); // Igual
+        System.out.println(valor1 != valor2); // Diferente
+        System.out.println(valor1 >= valor2); // Maior ou igual
+        System.out.println(valor1 <= valor2); // Menor ou igual
+        System.out.println(valor1 > valor2); // Maior
+        System.out.println(valor1 < valor2); // Menor
+    }
+}
