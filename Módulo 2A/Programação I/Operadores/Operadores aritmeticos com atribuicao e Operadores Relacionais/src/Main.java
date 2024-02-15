272904d76dff024f63e9f2b104a2c5786527839d
Gabriel <bielwdev@gmail.com>
2024-02-15 17:12:35 -0300

    Operadores aritméticos com atribuição e Operadores Relacionais

---
+++ Módulo 2A/Programação I/Operadores/Operadores aritmeticos com atribuicao e Operadores Relacionais/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        int numero;
+
+        numero = 6;
+        numero++;
+        numero +=1;
+        System.out.println(numero);
+
+        int numero2;
+
+        numero2 = 6;
+        numero2--;
+        numero -= 1;
+        System.out.println(numero2);
+
+        System.out.println(numero > 4);
+        System.out.println(numero >= 8);
+        System.out.println(numero == 7);
+        System.out.println(numero != 7);
+    }
+}
