bfb448fbee7b62a64a76df39023724f85e387a2b
Gabriel <bielwdev@gmail.com>
2024-02-16 13:22:06 -0300

    Instrução While

---
+++ Módulo 2A/Programação I/Instruções de Loop/Instrucao While/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while(numero <=5){
+            System.out.println(numero);
+            numero++;
+
+        }
+
+        System.out.println("Fim do primeiro while");
+
+        int numero2 = 9;
+
+        do {
+            System.out.println(numero2);
+            numero2++;
+        }while(numero2 <=5);
+
+    }
+}
