4d7b8ba25cc6f2b426e3095450e3f004cf779f15
Gabriel <bielwdev@gmail.com>
2024-02-16 22:21:01 -0300

    Array Bidimensional

---
+++ Módulo 2A/Programação I/String e Arrays/Array Bidimensional/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] matriz = new int[2][3];
+
+        matriz[0][0] = 1;
+        matriz[0][1] = 2;
+        matriz[0][2] = 3;
+        matriz[1][0] = 4;
+        matriz[1][1] = 5;
+        matriz[1][2] = 6;
+
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
