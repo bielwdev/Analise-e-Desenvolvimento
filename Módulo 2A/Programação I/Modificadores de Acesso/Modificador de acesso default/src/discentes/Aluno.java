052e173741009b3605f2e5a4b61d16b1e1d19ba7
Gabriel <bielwdev@gmail.com>
2024-03-06 06:10:45 -0300

    Modificador de acesso default

---
+++ Módulo 2A/Programação I/Modificadores de Acesso/Modificador de acesso default/src/discentes/Aluno.java
@@ -0,0 +1,31 @@
+package discentes;
+
+public class Aluno {
+    String nome;
+    double teste, prova;
+
+    public java.lang.String getNome() {
+        return nome;
+    }
+
+    public void setNome(java.lang.String nome) {
+        this.nome = nome;
+    }
+
+    public double getTeste() {
+        return teste;
+    }
+
+    public void setTeste(double teste) {
+        this.teste = teste;
+    }
+
+    public double getProva() {
+        return prova;
+    }
+
+    public void setProva(double prova) {
+        this.prova = prova;
+    }
+}
+
