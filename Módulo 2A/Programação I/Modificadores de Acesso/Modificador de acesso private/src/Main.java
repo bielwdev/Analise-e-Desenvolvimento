6eefc288e3dabb3ea2f7da197270fddb1b341af6
Gabriel <bielwdev@gmail.com>
2024-03-06 05:52:05 -0300

    Modificador de acesso private

---
+++ Módulo 2A/Programação I/Modificadores de Acesso/Modificador de acesso private/src/Main.java
@@ -0,0 +1,17 @@
+import discentes.Aluno;
+import discentes.AlunoSuperior;
+
+public class Main {
+    public static void main(String[] args) {
+        AlunoSuperior aluno = new AlunoSuperior();
+
+        //aluno.nome = "Gabriel";
+        aluno.setNome("Gabriel");
+        //aluno.teste = 9;
+        aluno.setTeste(9);
+        //aluno.prova = 10;
+        aluno.setProva(10);
+
+        System.out.print(aluno.getMedia());
+    }
+}
