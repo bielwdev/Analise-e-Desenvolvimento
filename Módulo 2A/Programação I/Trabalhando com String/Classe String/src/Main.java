e159111f8103c70aa4e72f4062309eb06abf92a4
Gabriel <bielwdev@gmail.com>
2024-03-06 06:26:10 -0300

    Classe String

---
+++ Módulo 2A/Programação I/Trabalhando com String/Classe String/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Gabriel";
+        String sobrenome = new String("Victor");
+
+        System.out.println(nome);
+        System.out.println(sobrenome);
+
+        System.out.println(nome.length());
+        System.out.println(sobrenome.length());
+
+    }
+}
---
+++ Módulo 2A/Programação I/Trabalhando com String/Classe String/src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+
+    String nome = "Gabriel";
+    String nomeGuerra = "Gabriel";
+    String sobrenome = new String("Gabriel");
+
+        if(nome==nomeGuerra)
+            System.out.println("Nomes iguais");
+        else
+                System.out.println("Nomes diferentes");
+
+        if (nome==sobrenome)
+            System.out.println("Nomes iguais");
+        else
+                System.out.println("Nomes diferentes");
+}
+}
