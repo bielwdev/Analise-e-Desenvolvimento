0bbbb1fa953125df9da3d39eed2c753e232403b8
Gabriel Victor <bielwdev@gmail.com>
2024-03-17 19:48:01 -0300

    TreeMap

---
+++ Módulo 2A/Programação I/Usando Collection - Parte 2/TreeMap/src/Principal.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        TreeMap<String, String> mapaNomes = new TreeMap<>();
+        mapaNomes.put("vava", "Oswaldo");
+        mapaNomes.put("gisa", "Gisele"); 
+        mapaNomes.put("gigi", "Giovanna"); 
+        mapaNomes.put ("giugiu", "Giulianna");
+
+        System.out.println("TreeMap de Nomes: " + mapaNomes);
+
+        System.out.println("Chaves: " + mapaNomes.keySet());
+        System.out.println("Valores: " + mapaNomes.values());
+        System.out.println("Chave/valor: " + mapaNomes.entrySet());
+    }
+}
